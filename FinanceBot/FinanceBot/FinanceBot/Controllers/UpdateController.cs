d383be0f7f2e9229b9298727bba340d56cfc8dab
igor_spirin <igor.r10@bk.ru>
2020-09-21 00:16:16 +0300

    feature(FinanceBot.Controllers) add in UpdateController.cs ParseCommand / ParseCommandException handlers

--- FinanceBot/FinanceBot/FinanceBot/Controllers/UpdateController.cs
+++ FinanceBot/FinanceBot/FinanceBot/Controllers/UpdateController.cs
@@ -4,6 +4,10 @@ using Microsoft.Extensions.Configuration;
 using Telegram.Bot.Types;
 using FinanceBot.Models;
 using FinanceBot.Models.Repository;
+using FinanceBot.Models.Commands.Utils;
+using System;
+using FinanceBot.Models.CommandsException;
+using FinanceBot.Models.Commands.ParseCommands;
 
 namespace FinanceBot.Controllers
 {
@@ -25,6 +29,8 @@ namespace FinanceBot.Controllers
             var commands = bot.Commands;
             var message = update.Message;
 
+            if (message == null) return Ok();
+
             var config = configuration.Get<Settings>();
             var client = await bot.Get(config);
 
@@ -35,12 +41,26 @@ namespace FinanceBot.Controllers
                     command.Execute(message, client, expenseRepository,
                         userAccountRepository, categoryRepository);
 
-                    break;
+                    return Ok();
                 }
-                //TODO: error handler
-
             }
 
+            var parseCommandDict = MessageParser
+                .ComposeParseCommandDict(message, userAccountRepository);
+
+            //TODO: Exeption handlers
+            try
+            {
+                var parsedCommand = MessageParser
+                    .ReadCommand(parseCommandDict, message);
+                parsedCommand.Execute(message, client);
+            }
+            catch (ParseCommandException e)
+            {
+                var badCommand = new BadCommand(e);
+                badCommand.Execute(message, client);
+            }
+            
             return Ok();
         }
     }
